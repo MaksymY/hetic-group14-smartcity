7b285e3068edcaba0e7cfdff9bd699095d90a0ef
MaksymY <maksym.yankivskyypro@gmail.com>
2020-03-01 23:56:51 +0100

    Clean folder

---
+++ src/data/LegendsPopulation.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: "FF6969",
+  },
+  {
+    id: "FF8080",
+  },
+  {
+    id: "FFA07D",
+  },
+  {
+    id: "FFBC7E",
+  },
+  {
+    id: "FFD47E",
+  },
+  {
+    id: "FFE664",
+  },
+  {
+    id: "F5F79C",
+  },
+];
